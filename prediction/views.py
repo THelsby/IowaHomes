9d687395fffe87076610d7e4e61fab3ceccbd3af
THelsby <thomas-helsby@hotmail.co.uk>
2019-08-13 07:30:51 +0100

    Prediction model ported to front page

--- prediction/views.py
+++ prediction/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import predictionForm
+
+
+# TODO use above package to require login to veiw homes @login_required
 
 
 # Create your views here.
 def home(request):
     return render(request, 'prediction/home.html')
+
+
+def prediction(request):
+    predForm = predictionForm()
+    return render(request, 'prediction/prediction.html', {'form': predForm})
